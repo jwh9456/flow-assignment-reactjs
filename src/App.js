e73b43512067a5b7586e4893fa068dc9ca9fbb87
jwh9456 <49363292+jwh9456@users.noreply.github.com>
2022-08-19 16:19:42 +0900

    css 수정

--- src/App.js
+++ src/App.js
@@ -18,16 +18,17 @@ function App() {
         <div className="wrapper">
           <div className={styles.banner}>◎ 파일 확장자 차단</div>
           <div className="description">파일 확장자에 따라 특정 형식의 파일을 첨부하거나 전송하지 못하도록 제한</div>
+              
           <div className={styles.ext_pinned}>
             <div className="ext_pinned_header">고정 확장자</div>
-            <div className="ext_pinned_elem">
+            <div className={styles.ext_pinned_elem}>
               <PinnedExtension extname="js"/>
 
             </div>
           </div>
           <div className={styles.ext_custom}>
             <div className="ext_custom_header">커스텀 확장자</div>
-            <div className="ext_custom_input_wrapper">
+            <div className={styles.ext_custom_input_wrapper}>
               <div className="ext_custom_input_textarea">
                 <input type='text' placeholder="확장자 입력" onChange={validateExtInput}/>&nbsp;
                 <button>+추가</button>
